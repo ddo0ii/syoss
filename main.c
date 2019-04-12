03bd286270f2c8c3cce84622cb837a63396aa7d3
soyhandong <21500035@handong.edu>
2019-04-12 11:20:53 +0900

    third commit

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void sub1();
 void sub2();
 void sub3();
@@ -10,3 +12,12 @@ int main(){
 	return 0;
 }
 
+void sub3(){
+	int i=0;
+	int j=0;l
+	for(i=0;i>9;i++){
+		for(j=0;j>9;j++){
+			printf("%d x %d = %d", i, j, i*j);
+		}
+	}
+}
